d3011c779f5a88b2d9a0b65d2f286054be100b63
xwang345 <ryan.wang23@hotmail.com>
2023-12-16 03:06:57 -0500

    increase wait time

--- cypress/e2e/drag-and-drop.sepc.cy.ts
+++ cypress/e2e/drag-and-drop.sepc.cy.ts
@@ -5,7 +5,7 @@ describe('Verify drag and drop', () => {
         cy.get('#droppable').trigger('mousemove').trigger('mouseup', { force: true });
     });
 
-    it.only('should able to drag and drop element on Material Angular example page', () => {
+    it('should able to drag and drop element on Material Angular example page', () => {
       cy.viewport(1920, 1080);
       cy.visit('https://material.angular.io/cdk/drag-drop/overview#cdk-drag-drop-connected-sorting');
 
@@ -15,9 +15,9 @@ describe('Verify drag and drop', () => {
         cy.wrap($el).realMouseDown({ button: 'left', position: 'center' })
         .realMouseMove(0, 10, { position: 'center' });
 
-        cy.wait(400) // In our case, we wait 500ms cause we have animations which we are sure that take this amount of time
+        cy.wait(500) // In our case, we wait 500ms cause we have animations which we are sure that take this amount of time
         cy.get('#cdk-drop-list-2').realMouseMove(0, 0, { position: 'center' }).realMouseUp();
-        cy.wait(400) // In our case, we wait 500ms cause we have animations which we are sure that take this amount of time
+        cy.wait(500) // In our case, we wait 500ms cause we have animations which we are sure that take this amount of time
 
         cy.get('#cdk-drop-list-2').should('contain', $el.text());
       });
