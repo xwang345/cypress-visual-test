1bb6407f78f3eee71afe3c9ab754d42d8ece091c
xwang345 <ryan.wang23@hotmail.com>
2023-12-21 21:23:29 -0500

    improving the reactive service with observables

--- recipe-book/src/app/app.component.ts
+++ recipe-book/src/app/app.component.ts
@@ -6,11 +6,4 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'recipe-book';
-
-  loadedFeature = 'recipe';
-
-  onNavigate(feature: string) {
-    this.loadedFeature = feature;
-  }
 }
