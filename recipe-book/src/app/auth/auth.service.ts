8fe71ee286cfac21e0bae5aeed3128697e445e43
xwang345 <ryan.wang23@hotmail.com>
2023-12-30 23:37:38 -0500

    fixing bug

--- recipe-book/src/app/auth/auth.service.ts
+++ recipe-book/src/app/auth/auth.service.ts
@@ -1,15 +1,16 @@
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { catchError } from 'rxjs/operators'; 
 import { throwError } from 'rxjs';
 
-interface AuthResponseData {
+export interface AuthResponseData {
   idToken: string;
   email: string;
   refreshToken: string;
   expiresIn: string;
-  localId: string;
+  localId: string,
+  registered?: boolean;
 }
 
 @Injectable({ providedIn: 'root' })
@@ -22,17 +23,40 @@ export class AuthService {
       email: email,
       password: password,
       returnSecureToken: true
-    }).pipe(catchError(errorRes => {
-      let errorMessage = 'An unknown error occurred!';
+    }).pipe(catchError(this.handleError));
+  }
 
-      if (!errorRes.error || !errorRes.error.error) {
-        return throwError(errorMessage);
-      }
-      switch (errorRes.error.error.message) {
-        case 'EMAIL_EXISTS':
-          errorMessage = 'This email exists already!';
-      }
+  login(email: string, password: string) {
+    return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDkStIYFgEKCTV7zu8bIg_9Of53VKwIpcg',
+    {
+      email: email,
+      password: password,
+      returnSecureToken: true
+    }).pipe(catchError(this.handleError));
+  }
+
+  private handleError(errorRes: HttpErrorResponse) {
+    let errorMessage = ('An unknown error occurred!');
+
+    if (!errorRes.error || !errorRes.error.error) {
       return throwError(errorMessage);
-    }));
+    }
+
+    switch (errorRes.error.error.message) {
+      case 'EMAIL_EXISTS':
+        errorMessage = 'This email exists already!';
+        break;
+      case 'EMAIL_NOT_FOUND':
+        errorMessage = 'This email does not exist!';
+        break;
+      case 'INVALID_LOGIN_CREDENTIALS':
+        errorMessage = 'This password is not correct!';
+        break;
+      case 'USER_DISABLED':
+        errorMessage = 'This user is disabled!';
+        break;
+    }
+
+    return throwError(errorMessage);
   }
 }
