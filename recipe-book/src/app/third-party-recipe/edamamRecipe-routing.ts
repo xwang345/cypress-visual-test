abeca7e8bb2687864df3721afd420a6ad6eb971f
xwang345 <ryan.wang23@hotmail.com>
2024-01-16 01:48:21 -0500

    add authGuard to protect edamam-recipe page

--- recipe-book/src/app/third-party-recipe/edamamRecipe-routing.ts
+++ recipe-book/src/app/third-party-recipe/edamamRecipe-routing.ts
@@ -2,12 +2,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { NgModule } from "@angular/core";
 import { EdamamRecipeComponent } from './edamam-recipe/edamam-recipe.component';
 import { EdamamRecipeResolverService } from './edamamRecipe-resolver.service';
+import { AuthGuard } from '../auth/auth.guard';
 
 const edamamRecipeRoutes: Routes = [
   { 
     path: 'edamam-recipe', 
     component: EdamamRecipeComponent, 
     // resolve: [EdamamRecipeResolverService],
+    canActivate: [AuthGuard],
     pathMatch: 'full'
   }
 ];
