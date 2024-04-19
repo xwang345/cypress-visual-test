7c34b76c303eb8ffeaf4500e8ac559bb1357cb32
xwang345 <ryan.wang23@hotmail.com>
2024-04-19 04:02:43 -0400

    disable the test in the test-add-ingredient-page.spec.cy.ts file

--- cypress/e2e/test-add-ingredient-page.spec.cy.ts
+++ cypress/e2e/test-add-ingredient-page.spec.cy.ts
@@ -1,90 +1,116 @@
-describe('shopping list page verification', () => {
-    it('should able to add recipes to the shopping list', () => {
-        cy.visit('localhost:4200/shopping-list')
-        cy.wait(300)
-        cy.get('#name').type('test');
-        cy.get('#amount').type('1');
-        cy.get('button:contains("Add")').click();
+xdescribe("shopping list page verification", () => {
+	it("should able to add recipes to the shopping list", () => {
+		cy.visit("localhost:4200/shopping-list");
+		cy.wait(300);
+		cy.get("#name").type("test");
+		cy.get("#amount").type("1");
+		cy.get('button:contains("Add")').click();
 
-        cy.get('.list-group-item').last().should('contain', 'test (1)');
-    });
+		cy.get(".list-group-item").last().should("contain", "test (1)");
+	});
 
-    it('should able to delete recipes from the shopping list', () => {
-        cy.visit('localhost:4200/shopping-list')
-        cy.wait(300)
-        cy.get('.list-group-item').last().click();
-       
-        cy.get('button:contains("Delete")').click();
+	it("should able to delete recipes from the shopping list", () => {
+		cy.visit("localhost:4200/shopping-list");
+		cy.wait(300);
+		cy.get(".list-group-item").last().click();
 
-        cy.get('.list-group-item').last().should('not.contain', ' Tomatoes (10)');
-    });
+		cy.get('button:contains("Delete")').click();
 
-    it('should able to edit recipes from the shopping list', () => {
-        cy.visit('localhost:4200/shopping-list')
-        cy.wait(300)
-        cy.get('.list-group-item').last().click();
-       
-        cy.get('#amount').clear();
-        cy.get('#amount').type('100');
-        cy.get('button:contains("Update")').click();
+		cy.get(".list-group-item").last().should("not.contain", " Tomatoes (10)");
+	});
 
-        cy.get('.list-group-item').last().should('contain', 'Tomatoes (100)');
-    });
+	it("should able to edit recipes from the shopping list", () => {
+		cy.visit("localhost:4200/shopping-list");
+		cy.wait(300);
+		cy.get(".list-group-item").last().click();
 
-    it('should able to add new recipe', () => {
-        cy.visit('localhost:4200/recipes')
-        cy.wait(300)
-        cy.get('button:contains("New Recipe")').click();
-        cy.get('#name').type('test');
-        cy.get('#imagePath').type('src="https://www.recipetineats.com/wp-content/uploads/2019/04/Beef-Pho_4.jpg"');
-        cy.get('#description').type("Lorem Ipsum is simply dummy text of the printing and typesetting industry.");
-        cy.get('button:contains("Add Ingredient")').click();
-        
+		cy.get("#amount").clear();
+		cy.get("#amount").type("100");
+		cy.get('button:contains("Update")').click();
 
-        cy.get('[formarrayname="ingredients"]').find('[formcontrolname="name"]').type('test');
-        cy.get('[formarrayname="ingredients"]').find('[formcontrolname="amount"]').type('123');
+		cy.get(".list-group-item").last().should("contain", "Tomatoes (100)");
+	});
 
-        cy.get('button:contains("Save")').click();
+	it("should able to add new recipe", () => {
+		cy.visit("localhost:4200/recipes");
+		cy.wait(300);
+		cy.get('button:contains("New Recipe")').click();
+		cy.get("#name").type("test");
+		cy.get("#imagePath").type(
+			'src="https://www.recipetineats.com/wp-content/uploads/2019/04/Beef-Pho_4.jpg"',
+		);
+		cy.get("#description").type(
+			"Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+		);
+		cy.get('button:contains("Add Ingredient")').click();
 
-        cy.get('.list-group-item').last().should('contain', 'test');
-        cy.get('.list-group-item').last().click();
-    });
+		cy.get('[formarrayname="ingredients"]')
+			.find('[formcontrolname="name"]')
+			.type("test");
+		cy.get('[formarrayname="ingredients"]')
+			.find('[formcontrolname="amount"]')
+			.type("123");
 
-    it.only('should able to login to the recipe book', () => {
-        cy.viewport('macbook-16');
-        cy.visit('localhost:4200/auth')
-        cy.wait(300)
-        cy.get('#email').type('test@test.com');
-        cy.get('#password').type('test123');
-        cy.get('button:contains("Login")').click();
+		cy.get('button:contains("Save")').click();
 
-         cy.get('button:contains("New Recipe")').click();
-        cy.get('#name').type('Pho');
-        cy.get('#imagePath').type('https://www.recipetineats.com/wp-content/uploads/2019/04/Beef-Pho_4.jpg');
-        cy.get('#description').type("Experience the magic of one of the greatest noodle soups in the world with this easy to follow traditional Vietnamese Pho recipe!");
-        cy.get('button:contains("Add Ingredient")').click();
-        
+		cy.get(".list-group-item").last().should("contain", "test");
+		cy.get(".list-group-item").last().click();
+	});
 
-        cy.get('[formarrayname="ingredients"]').find('[formcontrolname="name"]').type('large onions');
-        cy.get('[formarrayname="ingredients"]').find('[formcontrolname="amount"]').type('2');
+	it.only("should able to login to the recipe book", () => {
+		cy.viewport("macbook-16");
+		cy.visit("localhost:4200/auth");
+		cy.wait(300);
+		cy.get("#email").type("test@test.com");
+		cy.get("#password").type("test123");
+		cy.get('button:contains("Login")').click();
 
-        cy.get('button:contains("Add Ingredient")').click();
+		cy.get('button:contains("New Recipe")').click();
+		cy.get("#name").type("Pho");
+		cy.get("#imagePath").type(
+			"https://www.recipetineats.com/wp-content/uploads/2019/04/Beef-Pho_4.jpg",
+		);
+		cy.get("#description").type(
+			"Experience the magic of one of the greatest noodle soups in the world with this easy to follow traditional Vietnamese Pho recipe!",
+		);
+		cy.get('button:contains("Add Ingredient")').click();
 
-        cy.get('[formarrayname="ingredients"]').find('[formcontrolname="name"]').last().type('star anise');
-        cy.get('[formarrayname="ingredients"]').last().find('[formcontrolname="amount"]').last().type('8');
+		cy.get('[formarrayname="ingredients"]')
+			.find('[formcontrolname="name"]')
+			.type("large onions");
+		cy.get('[formarrayname="ingredients"]')
+			.find('[formcontrolname="amount"]')
+			.type("2");
 
-        cy.get('button:contains("Add Ingredient")').click();
+		cy.get('button:contains("Add Ingredient")').click();
 
-        cy.get('[formarrayname="ingredients"]').find('[formcontrolname="name"]').last().type('beef brisket');
-        cy.get('[formarrayname="ingredients"]').find('[formcontrolname="amount"]').last().type('3');
+		cy.get('[formarrayname="ingredients"]')
+			.find('[formcontrolname="name"]')
+			.last()
+			.type("star anise");
+		cy.get('[formarrayname="ingredients"]')
+			.last()
+			.find('[formcontrolname="amount"]')
+			.last()
+			.type("8");
 
+		cy.get('button:contains("Add Ingredient")').click();
 
-        cy.get('button:contains("Save")').click();
+		cy.get('[formarrayname="ingredients"]')
+			.find('[formcontrolname="name"]')
+			.last()
+			.type("beef brisket");
+		cy.get('[formarrayname="ingredients"]')
+			.find('[formcontrolname="amount"]')
+			.last()
+			.type("3");
 
-        cy.get('.list-group-item').last().should('contain', 'Pho');
-        cy.get('.list-group-item').last().click();
+		cy.get('button:contains("Save")').click();
 
-        cy.get('.dropdown').click();
-        cy.get('a:contains("Save Data")').click();
-    });
+		cy.get(".list-group-item").last().should("contain", "Pho");
+		cy.get(".list-group-item").last().click();
+
+		cy.get(".dropdown").click();
+		cy.get('a:contains("Save Data")').click();
+	});
 });
